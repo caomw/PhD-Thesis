de81e0fe54d11bb021f41875e0ab56ea392fc14e
img.science <img.science@17faa34a-554f-a99d-949d-d413bc473c9c>
2012-06-25 18:21:26 +0000

    Switched to BSD

--- imagesci/src/org/imagesci/springls/PrefixScanCPU.cl
+++ imagesci/src/org/imagesci/springls/PrefixScanCPU.cl
@@ -1,4 +1,29 @@
-
+/**
+ *       Java Image Science Toolkit
+ *                  --- 
+ *     Multi-Object Image Segmentation
+ *
+ * Copyright(C) 2012 Blake Lucas (img.science@gmail.com)
+ * All rights reserved.
+ * 
+ * Center for Computer-Integrated Surgical Systems and Technology &
+ * Johns Hopkins Applied Physics Laboratory &
+ * The Johns Hopkins University
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the The Johns Hopkins University.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @author Blake Lucas (img.science@gmail.com)
+ */
 
 kernel void prefixScanSubList(global int* inBuffer,global int* outBuffer,int stride,int elements){
 	uint id=get_global_id(0);
--- imagesci/src/org/imagesci/utility/IsoContourGenerator.java
+++ imagesci/src/org/imagesci/utility/IsoContourGenerator.java
@@ -4,36 +4,27 @@
  *     Multi-Object Image Segmentation
  *
  * Copyright(C) 2012 Blake Lucas (img.science@gmail.com)
+ * All rights reserved.
  * 
  * Center for Computer-Integrated Surgical Systems and Technology &
  * Johns Hopkins Applied Physics Laboratory &
  * The Johns Hopkins University
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.  The license is available for reading at:
- * http://www.gnu.org/copyleft/lgpl.html
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the The Johns Hopkins University.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @author Blake Lucas (img.science@gmail.com)
  */
 package org.imagesci.utility;
-
-/**
- * ImageSci Toolkit
- *
- * Center for Computer-Integrated Surgical Systems and Technology &
- * Johns Hopkins Applied Physics Laboratory &
- * The Johns Hopkins University
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.  The license is available for reading at:
- * http://www.gnu.org/copyleft/lgpl.html
- *
- * @author Blake Lucas
- */
 
 import java.util.Hashtable;
 import java.util.LinkedList;
