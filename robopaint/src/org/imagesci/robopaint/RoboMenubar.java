38477bfdfa71c73ea4c1d75f18b49ac036e69805
tifany.yung@gmail.com <tifany.yung@gmail.com@17faa34a-554f-a99d-949d-d413bc473c9c>
2012-06-30 23:01:52 +0000

    Image, Geometry, and Paint controls complete. File menu complete.

--- robopaint/src/org/imagesci/robopaint/RoboMenubar.java
+++ robopaint/src/org/imagesci/robopaint/RoboMenubar.java
@@ -1,10 +1,12 @@
 package org.imagesci.robopaint;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
@@ -12,6 +14,8 @@ import org.eclipse.swt.widgets.Shell;
 
 public class RoboMenubar {
 	public RoboMenubar(Shell parent) {
+		
+		final Shell shell = parent.getShell();
 		Menu mbar = new Menu(parent, SWT.BAR);
 
 		MenuItem fileMenu = new MenuItem(mbar, SWT.CASCADE);
@@ -22,38 +26,49 @@ public class RoboMenubar {
 		MenuItem openItem = new MenuItem(fileSubMenu, SWT.PUSH);
 		openItem.setText("&Open\tCtrl+O");
 		openItem.setAccelerator(SWT.MOD1 + 'O');
-		openItem.addSelectionListener(new SelectionListener() {
+		openItem.addSelectionListener(new SelectionAdapter() {
 			
 			public void widgetSelected(SelectionEvent event) {
 				
-				
-			}
-			
-			public void widgetDefaultSelected(SelectionEvent event) {
-				
+				FileDialog fileDialog = new FileDialog(shell, SWT.OPEN);
+				fileDialog.setText("Open");
+				fileDialog.setFilterPath("C:/");
+				String[] filterExtensions = {"*.img", "*.hdr", "*.nii"};
+				fileDialog.setFilterExtensions(filterExtensions);
+				fileDialog.open();
 			}
 		});
 		
 		MenuItem saveItem = new MenuItem(fileSubMenu, SWT.PUSH);
 		saveItem.setText("&Save\tCtrl+S");
 		saveItem.setAccelerator(SWT.MOD1 + 'S');
-		saveItem.addListener(SWT.Selection, new Listener() {
+		saveItem.addSelectionListener(new SelectionAdapter() {
 			
-			public void handleEvent(Event e) {
-				
-				System.out.println("SAVE!");
-			}
 		});
 		
 		MenuItem saveAsItem = new MenuItem(fileSubMenu, SWT.PUSH);
 		saveAsItem.setText("&Save As\tCtrl+SHIFT+S");
 		saveAsItem.setAccelerator(SWT.MOD1 + SWT.MOD2 +'S');
-		saveAsItem.addListener(SWT.Selection, new Listener() {
+		saveAsItem.addSelectionListener(new SelectionAdapter() {
 			
-			public void handleEvent(Event e) {
+			public void widgetSelected(SelectionEvent event) {
 				
-				System.out.println("SAVE AS!");
+				FileDialog fileDialog = new FileDialog(shell, SWT.OPEN);
+				fileDialog.setText("Save As");
+				fileDialog.setFilterPath("C:/");
+				String[] filterExtensions = {"*.img", "*.hdr", "*.nii"};
+				fileDialog.setFilterExtensions(filterExtensions);
+				fileDialog.open();
 			}
+		});
+		
+		MenuItem separator = new MenuItem(fileSubMenu, SWT.SEPARATOR);
+		
+		MenuItem quitItem = new MenuItem(fileSubMenu, SWT.PUSH);
+		quitItem.setText("&Quit\tCtrl+Q");
+		quitItem.setAccelerator(SWT.MOD1 + 'Q');
+		quitItem.addSelectionListener(new SelectionAdapter() {
+			
 		});
 		
 		parent.setMenuBar(mbar);
