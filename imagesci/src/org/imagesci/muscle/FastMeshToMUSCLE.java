d3270d12ff60d01dda543e74f28f57756909a6fb
img.science@gmail.com <img.science@gmail.com@17faa34a-554f-a99d-949d-d413bc473c9c>
2012-07-24 00:18:20 +0000

    removed unused function

--- imagesci/src/org/imagesci/muscle/FastMeshToMUSCLE.java
+++ imagesci/src/org/imagesci/muscle/FastMeshToMUSCLE.java
@@ -240,9 +240,10 @@ public class FastMeshToMUSCLE extends AbstractCalculation {
 		distFieldBuffer.rewind();
 
 		if (smoothingIters > 0) {
+			System.out.println("Smoothing ...");
 			MuscleEvolveLevelSet3D mogac = new MuscleEvolveLevelSet3D(commons,
 					labelImage, smoothWeight);
-			mogac.setPreserveTopology(true);
+			mogac.setPreserveTopology(false);
 			mogac.setMaxIterations(smoothingIters);
 			mogac.init(distField, labelImage, false);
 			mogac.evolve();
@@ -257,82 +258,6 @@ public class FastMeshToMUSCLE extends AbstractCalculation {
 				+ " sec");
 		commons.dispose();
 		markCompleted();
-	}
-
-	/**
-	 * Convert unsigned to signed.
-	 * 
-	 * @param commons
-	 *            the commons
-	 * @param tmpSignedLevelSet
-	 *            the tmp signed level set
-	 * @param extend
-	 *            the extend
-	 */
-	private void convertUnsignedToSigned(MuscleCommon3D commons,
-			CLBuffer<FloatBuffer> tmpSignedLevelSet, boolean extend) {
-		CLKernel extendDistanceField = commons.kernelMap
-				.get(SpringlsCommon3D.EXTEND_DISTANCE_FIELD);
-
-		// Init to unlabeled
-		final CLKernel initSignedLevelSet = commons.kernelMap
-				.get("initSignedLevelSet");
-		final CLKernel erodeLevelSet = commons.kernelMap.get("erodeLevelSet");
-		final CLKernel multiplyLevelSets = commons.kernelMap
-				.get("multiplyLevelSets");
-		initSignedLevelSet.putArg(commons.signedLevelSetBuffer).rewind();
-		commons.queue.put1DRangeKernel(
-				initSignedLevelSet,
-				0,
-				SpringlsCommon3D.roundToWorkgroupPower(commons.rows
-						* commons.cols * commons.slices),
-				SpringlsCommon3D.WORKGROUP_SIZE);
-		int blockSize = Math.max(Math.max(commons.rows, commons.cols),
-				commons.slices) / (MAX_CYCLES);
-		CLBuffer<FloatBuffer> buffIn = commons.signedLevelSetBuffer;
-		CLBuffer<FloatBuffer> buffOut = tmpSignedLevelSet;
-		int cycles = 0;
-		int lastBlockSize = Math.max(Math.max(commons.rows, commons.cols),
-				commons.slices);
-
-		while (blockSize >= 1) {
-			cycles = lastBlockSize / (blockSize);
-			int globalSize = commons.rows * commons.cols * commons.slices
-					/ (blockSize * blockSize * blockSize);
-			for (int i = 0; i < 2 * cycles; i++) {
-				erodeLevelSet
-						.putArgs(commons.unsignedLevelSetBuffer, buffIn,
-								buffOut).putArg(blockSize).rewind();
-
-				commons.queue.put1DRangeKernel(erodeLevelSet, 0,
-						SpringlsCommon3D.roundToWorkgroupPower(globalSize),
-						Math.min(globalSize, SpringlsCommon3D.WORKGROUP_SIZE));
-				CLBuffer<FloatBuffer> tmp = buffOut;
-				buffOut = buffIn;
-				buffIn = tmp;
-			}
-			lastBlockSize = blockSize;
-			blockSize /= 2;
-
-		}
-		// End
-		multiplyLevelSets.putArgs(commons.unsignedLevelSetBuffer,
-				commons.signedLevelSetBuffer).rewind();
-		commons.queue.put1DRangeKernel(
-				multiplyLevelSets,
-				0,
-				SpringlsCommon3D.roundToWorkgroupPower(commons.rows
-						* commons.cols * commons.slices),
-				SpringlsCommon3D.WORKGROUP_SIZE);
-		if (extend) {
-			for (int i = DISTANCE_FIELD_EXTENT - 1; i < 4 * DISTANCE_FIELD_EXTENT; i++) {
-				extendDistanceField.putArgs(commons.unsignedLevelSetBuffer)
-						.putArg(i).rewind();
-				commons.queue.put1DRangeKernel(extendDistanceField, 0,
-						commons.rows * commons.cols * commons.slices,
-						SpringlsCommon3D.WORKGROUP_SIZE);
-			}
-		}
 	}
 
 	/**
