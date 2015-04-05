8dcfd47da7aeab48ce35cbc639bbd3aef68daacd
bclucas <blake.c.lucas@gmail.com>
2015-04-04 18:05:01 -0700

    Added scroll /translate

--- GeometryPlayground/src/org/imagesci/playground/SpringlFluid2D.java
+++ GeometryPlayground/src/org/imagesci/playground/SpringlFluid2D.java
@@ -3,8 +3,13 @@ import java.io.File;
 
 import javax.vecmath.Point2f;
 
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.Transform;
+
 import processing.core.PApplet;
 import processing.core.PImage;
+import processing.core.PMatrix2D;
+import processing.core.PVector;
+import processing.event.MouseEvent;
 import edu.jhu.ece.iacl.jist.io.PImageReaderWriter;
 import edu.jhu.ece.iacl.jist.structures.image.ImageDataFloat;
 
@@ -18,6 +23,8 @@ public class SpringlFluid2D extends PApplet {
 
 PImage backgroundImage;
 ParticleVolume particleGrid;
+PMatrix2D Pose;
+float lastMouseX=0,lastMouseY=0;
 public void setup() {
   size(800,800);
   background(102);
@@ -37,14 +44,50 @@ public void setup() {
 	}
 	ImageDataFloat levelSet = df.solve(sourceImage, 15.0);
 	particleGrid=new ParticleVolume(levelSet);
-	
+	Pose=new PMatrix2D();
+	Pose.reset();
+}
+public void mousePressed(){
+	if(mouseButton==LEFT){
+		float oldScale=Pose.m00;
+		lastMouseX=mouseX;
+		lastMouseY=mouseY;
+	}
+}
+public void mouseDragged(){
+	if(mouseButton==RIGHT){
+		float oldScale=Pose.m00;
+		Pose.translate((mouseX-pmouseX)/oldScale,(mouseY-pmouseY)/oldScale);
+	}
+	if(mouseButton==LEFT){
+		float scale=1.0f+10.0f*(mouseY-pmouseY)/(float)height;
+		
+		Pose.translate(lastMouseX, lastMouseY);
+		Pose.scale(scale,scale);
+		Pose.translate(-lastMouseX, -lastMouseY);
+		/*
+		System.out.println("Pose =["+lastMouseX+","+lastMouseY+"]\n"+
+			Pose.m00+" "+Pose.m01+" "+Pose.m02+"\n"+
+			Pose.m10+" "+Pose.m11+" "+Pose.m12+"\n");
+		*/
+	}
+			
 }
 
+public void mouseWheel(MouseEvent event) {
+	float e = event.getCount();
+	float scale=1.0f+30.0f*Math.signum(e)/(float)height;		
+	Pose.translate(mouseX, mouseY);
+	Pose.scale(scale,scale);
+	Pose.translate(-mouseX, -mouseY);
+}
 public void draw() {
- 
+
   fill(255);
   noStroke();
   rect(0,0,width,height);
+  pushMatrix();
+  applyMatrix(Pose);
   
   image(backgroundImage,-0.5f*width/(float)backgroundImage.width,-0.5f*height/(float)backgroundImage.height, width,height);
   ellipseMode(RADIUS); 
@@ -54,12 +97,12 @@ public void draw() {
   noFill();
   strokeWeight(1.0f);
   stroke(128,128,128,128);
-  for (int i = 0; i < backgroundImage.width; i++) {
-	  line(scaleX*i,0,scaleX*i,scaleY*height);
+  for (int i = 0; i < backgroundImage.width+1; i++) {
+	  line(scaleX*i,0,scaleX*i,scaleY*backgroundImage.height);
   }
   
-  for (int j = 0; j < backgroundImage.height; j++) {
-	  line(0,scaleY*j,scaleX*width,scaleY*j);
+  for (int j = 0; j < backgroundImage.height+1; j++) {
+	  line(0,scaleY*j,scaleX*backgroundImage.width,scaleY*j);
   }
   smooth(8);
   fill(255,64,64,128);
@@ -98,8 +141,11 @@ public void draw() {
   for(FluidInterfaceParticle ip:particleGrid.interfaceParticles){
 	  point(scaleX*ip.x,scaleY*ip.y);
   }
+  popMatrix();
   
-
+ // stroke(0);
+//  fill(0);
+ // text("Scale: "+String.format("%4.1f",scale)+" ("+transX+","+transY+")",5,15,0);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "org.imagesci.playground.SpringlFluid2D" };
